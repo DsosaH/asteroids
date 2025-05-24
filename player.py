cad8a191076b9414c5f9393465244c67000a6602
DsosaH <dsosacontact@gmail.com>
2025-05-23 18:35:45 -0600

    added groups for sprites

--- player.py
+++ player.py
@@ -2,9 +2,11 @@ from circleshape import CircleShape
 from constants import*
 import pygame
 class Player(CircleShape):
+    
     def __init__(self, x, y,):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
