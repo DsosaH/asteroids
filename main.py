3747f7b5ec37a0723c061a0c6ae63403957e20e1
DsosaH <dsosacontact@gmail.com>
2025-05-23 21:34:46 -0600

    added collision

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init
@@ -31,7 +32,11 @@ def main():
         for draw in drawable:
             draw.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+        for asteroid in asteroids:
+            if asteroid.check_for_collision(player):
+                print("Game over!")
+                sys.exit()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
